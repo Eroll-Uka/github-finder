6cfaeec7004b6f04dddc4c7696b6926ccee1f110
Eroll Uka <erollfaceit@gmail.com>
2022-05-29 22:09:47 +0200

    Added footer + routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
 import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
@@ -7,7 +11,15 @@ function App() {
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
 
-        <main> Content </main>
+        <main className='container mx-auto px-3 pb-12'> 
+        <Routes>
+        <Route path='/' element = {<Home />} />
+        <Route path='/about' element = {<About />} />
+        <Route path='/notfound' element = {<NotFound />} />
+        <Route path='/*' element = {<NotFound />} />
+        </Routes>
+        <Footer />
+        </main>
       </div>
     </Router>
   );
