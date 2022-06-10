e8431b844931d0629a488f487575ca1b5c95b510
Eroll Uka <erollfaceit@gmail.com>
2022-06-10 21:37:20 +0200

    Cleaned up fetch users

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
-import {useEffect , useContext} from 'react'
+import {useContext} from 'react'
 import Spinner from '../layout/Spinner'
 import UserItem from './UserItem'
 import GithubContext from '../../context/github/GithubContext'
 
 function UserResults() {
 
-    const {users,loading,fetchUsers} = useContext(GithubContext)
+    const {users,loading} = useContext(GithubContext)
 
-useEffect(() => {
-    fetchUsers()
-}, [])
 
     if(!loading) {
      return (
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -8,12 +8,13 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 export const GithubProvider = ({children}) => {
   const initialState = {
       users: [],
-      loading: true
+      loading: false,
   }
 
   const [state, dispatch] = useReducer(githubReducer,initialState)
     
     const fetchUsers = async () => {
+        setLoading()
         const response = await fetch(`${GITHUB_URL}/users`, {
             headers : {
             Authorization :`token ${GITHUB_TOKEN}`,
@@ -28,6 +29,11 @@ export const GithubProvider = ({children}) => {
             })
         
     }
+
+
+    //Set Loading
+    const setLoading = () => dispatch({type: 'SET_LOADING'})
+
     return <GithubContext.Provider value={{
         users: state.users,
         loading: state.loading,
