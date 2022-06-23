84e9aed0f699bddeb69c5d58628ff0e5288ac797
Eroll Uka <erollfaceit@gmail.com>
2022-06-23 22:28:08 +0200

    updated spread

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -66,9 +66,7 @@ export const GithubProvider = ({children}) => {
 
     return <GithubContext.Provider 
     value={{
-        users: state.users,
-        loading: state.loading,
-        user: state.user,
+       ...state,
         searchUsers,
         clearUsers,
         getUser,
