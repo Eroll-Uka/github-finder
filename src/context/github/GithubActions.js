8db3d8a1351c547d24d160ef49ed47200e9db862
Eroll Uka <erollfaceit@gmail.com>
2022-06-28 13:24:33 +0200

    Project Finished

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,21 +1,20 @@
+import axios from 'axios'
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+
+
+const github = axios.create({
+    baseURL: GITHUB_URL,
+    headers: { Authorization :`token ${GITHUB_TOKEN}` }
+})
 
 export const searchUsers = async (text) => {
     const params = new URLSearchParams({
         q: text
     })
-    
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-        headers : {
-        Authorization :`token ${GITHUB_TOKEN}`,
-        },
-    })
 
-    const {items} = await response.json()
-
-       return items
-    
+    const response = await github.get(`/search/users?${params}`)
+    return response.data.items
 }
  //get single user 
  export const getUser = async (login) => {
